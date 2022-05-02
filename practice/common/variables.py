05c3c432694baef3b7e5c1fc6056673f27958d34
Spawn4real <m.demchenko2001@gmail.com>
2022-05-02 21:23:13 +0700

    Lesson_3. Create first server_client_app.

---
+++ practice/common/variables.py
@@ -0,0 +1,27 @@
+"""ПЕРЕМЕННЫЕ ПО УМОЛЧАНИЮ"""
+
+# Порт по умолчанию
+DEFAULT_PORT = 6666
+# IP Адрес по умолчанию для клиента
+DEFAULT_IP = '127.0.0.1'
+# Максимальное количество подключений
+MAX_CONNECTIONS = 10
+# Максимальная длинна байтового сообщения
+MAX_PACKAGES_LEN = 1024
+# Кодировка
+ENCODING = 'utf-8'
+
+
+ACTION = 'action'
+
+PRESENCE = 'presence'
+
+TIME = 'time'
+
+USER = 'user'
+
+ACCOUNT_NAME = 'account_name'
+
+RESPONSE = 'responce'
+
+ERROR = 'error'
---
+++ practice/server.py
@@ -0,0 +1,63 @@
+"""СЕРВЕРНАЯ ЧАСТЬ"""
+
+import sys
+import json
+import socket
+from practice.common.variables import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, RESPONSE, ERROR, \
+    DEFAULT_PORT, MAX_CONNECTIONS
+from practice.common.utils import send_messages, get_messages
+
+
+def procces_client_message(message):
+    if ACTION in message and message[ACTION] == PRESENCE and TIME in message and USER in message \
+            and message[USER][ACCOUNT_NAME] == 'Guest':
+        return {RESPONSE: 200}
+    return f'400 : {message[ERROR]}'
+
+
+def main():
+    try:
+        if '-p' in sys.argv:
+            listen_port = int(sys.argv[sys.argv.index('-p') + 1])
+        else:
+            listen_port = DEFAULT_PORT
+        if listen_port < 1024 or listen_port > 65535:
+            raise ValueError
+    except IndexError:
+        print("После параметра -\'p\' - необходимо указать порт.")
+        sys.exit(1)
+    except ValueError:
+        print('Номер порта может быть только в диапозоне от 1024 жл 65535')
+        sys.exit(1)
+
+    try:
+        if '-a' in sys.argv:
+            listen_adress = sys.argv[sys.argv.index('-a') + 1]
+        else:
+            listen_adress = ''
+    except IndexError:
+        print('После параметра -\'a\' необходимо указать адрес, который будет слушать сервер.')
+        sys.exit(1)
+
+    transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    transport.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    transport.bind((listen_adress, listen_port))
+
+    transport.listen(MAX_CONNECTIONS)
+
+    while True:
+        client, client_adress = transport.accept()
+        try:
+            message_from_client = get_messages(client)
+            print(message_from_client)
+            response = procces_client_message(message_from_client)
+            send_messages(client, response)
+            client.close()
+        except (ValueError, json.JSONDecodeError):
+            print('Принято некорректное сообщение от клиента.')
+            client.close()
+
+
+if __name__ == '__main__':
+    main()
+
