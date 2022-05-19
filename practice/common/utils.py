f5996c9e8a2007828d652b3ce377eb14ec3ead05
maxim <m.demchenko2001@gmail.com>
2022-05-19 20:02:47 +0700

    lesson_6

--- practice/common/utils.py
+++ practice/common/utils.py
@@ -18,9 +18,9 @@ def get_messages(client):
             response = json.loads(json_response)
             if isinstance(response, dict):
                 return response
-            raise ValueError
-        raise ValueError
-    raise ValueError
+            raise IncorrectDataRecivedError
+        raise IncorrectDataRecivedError
+    raise IncorrectDataRecivedError
 
 
 # Функция отправки сообщения и кодирования. Принимает в себя словарь, если нет выводит TypeError,
@@ -28,7 +28,7 @@ def get_messages(client):
 @log
 def send_messages(sock, message):
     if not isinstance(message, dict):
-        raise TypeError
+        raise NonDictInputError
     js_message = json.dumps(message)
     encoding_messege = js_message.encode(ENCODING)
     sock.send(encoding_messege)
